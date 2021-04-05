e6cf3cec78995270204dcbb65024ca6eeec3a72a
Juan Jose Jolon <jjolong@miumg.edu.gt>
2021-04-05 08:40:27 -0600

    Update sendMessagePrivate.php

--- app/Mail/sendMessagePrivate.php
+++ app/Mail/sendMessagePrivate.php
@@ -7,7 +7,7 @@ use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Contracts\Queue\ShouldQueue;
 
-class sendMessagePrivate extends Mailable
+class sendMessagePrivate extends Mailable implements ShouldQueue
 {
     use Queueable, SerializesModels;
 
